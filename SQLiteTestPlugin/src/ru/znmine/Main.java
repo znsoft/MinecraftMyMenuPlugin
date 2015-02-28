6c404553016ed944af97b880c5caa3a3d8d1b143
znsoft <zn@bk.ru>
2015-02-28 20:34:34 +1000

    добавил возможность введения нескольких команд на одну иконку, команды вводятся через ";"

--- SQLiteTestPlugin/src/ru/znmine/Main.java
+++ SQLiteTestPlugin/src/ru/znmine/Main.java
@@ -5,10 +5,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 //import java.util.logging.Logger;
 
-
-
 import java.util.logging.Logger;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -18,31 +17,47 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import com.droppages.Skepter.SQL.SQLite;
 
-public class Main extends JavaPlugin  {
+public class Main extends JavaPlugin {
 
 	public Logger log = Logger.getLogger("Minecraft");
 	public SQLite sqlite;
 	public IconMenu menu;
-	public HashMap<Player, СостояниеИгрока> состояниеИгрока = new HashMap<Player, СостояниеИгрока>(); 
-	public HashMap<ItemStack, Менюшка> МенюИгрока ; 
-
+	public HashMap<Player, СостояниеИгрока> состояниеИгрока = new HashMap<Player, СостояниеИгрока>();
+	public HashMap<ItemStack, Менюшка> МенюИгрока;
 
 	public void onEnable() {
 		File file = new File(getDataFolder(), "znmine.db");
 		sqlite = new SQLite(file);
 		sqlite.open();
-		EvntPlay ОбработчикСобытий =  new EvntPlay(this);
+		final Main m = this;
+		EvntPlay ОбработчикСобытий = new EvntPlay(this);
 		sqlite.execute("create table if not exists Players ( playername primary key , op , privileges , money , ip , chatlog , commands , level );");
 		sqlite.execute("create table if not exists PlayerMenu ( playername , command , name , icon , position );");
-		menu = new IconMenu("Player Menu", 
+		menu = new IconMenu("Player Menu",
 				new IconMenu.OptionClickEventHandler() {
 					@Override
 					public void onOptionClick(IconMenu.OptionClickEvent event) {
-						try{
-						String cmd = event.getItemMeta().getLore().get(0);
-						event.getPlayer().performCommand(cmd);
-						event.setWillClose(true);
-						}catch(Exception e){}
+						try {
+							String cmds = event.getItemMeta().getLore().get(0);
+							String[] МассивКоманд = cmds.split(";");
+							final Player p = event.getPlayer();
+							if (МассивКоманд.length == 1) {
+								p.performCommand(cmds);
+							} else {
+								for (int i = 0; i < МассивКоманд.length; i++) {
+									final String Команда = МассивКоманд[i];
+									Bukkit.getScheduler()
+											.scheduleSyncDelayedTask(m,
+													new Runnable() {
+														public void run() {
+															p.performCommand(Команда);
+														}
+													}, i * 2);
+								}
+							}
+							event.setWillClose(true);
+						} catch (Exception e) {
+						}
 					}
 				}, this);
 		getServer().getPluginManager().registerEvents(ОбработчикСобытий, this);
@@ -53,52 +68,59 @@ public class Main extends JavaPlugin  {
 	public void onDisable() {
 		sqlite.close();
 	}
-	
 
-
-	
-	
 	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+	public boolean onCommand(CommandSender sender, Command cmd, String label,
+			String[] args) {
 		if (!(sender instanceof Player)) {
 			sender.sendMessage("znmine:This command can only be run by a player.");
 			return false;
-		} 
-		Player p = (Player)sender;
+		}
+		Player p = (Player) sender;
 		СостояниеИгрока СИ = состояниеИгрока.get(p);
 		String command = cmd.getName().toLowerCase();
-		if (command.equalsIgnoreCase("mymenu")||command.equalsIgnoreCase("менюшка")) { // If the player typed /basic then do the following...
-			if(args.length==0)return false;
-			if(args.length>1 && (args[0].equalsIgnoreCase("add")||args[0].toLowerCase().equalsIgnoreCase("добавить"))){
+		if (command.equalsIgnoreCase("mymenu")
+				|| command.equalsIgnoreCase("менюшка")) { // If the player typed
+															// /basic then do
+															// the following...
+			if (args.length == 0)
+				return false;
+			if (args.length > 1
+					&& (args[0].equalsIgnoreCase("add") || args[0]
+							.toLowerCase().equalsIgnoreCase("добавить"))) {
 				String s = args[1];
-				for(int i = 2;i<=args.length-1;i++)s = s + ' ' + args[i];
-				СИ.ДобавитьЭлементМеню(args[1],s);
+				for (int i = 2; i <= args.length - 1; i++)
+					s = s + ' ' + args[i];
+				СИ.ДобавитьЭлементМеню(args[1], s);
 				ДобавитьМенювИБД(args[1], p, s);
-				sender.sendMessage("Команда добавлена к вам в меню");  
-				}
-			
-			if(args[0].equalsIgnoreCase("remove")||args[0].toLowerCase().equalsIgnoreCase("удалить")){
-				if(args.length==1){
+				sender.sendMessage("Команда добавлена к вам в меню");
+			}
+
+			if (args[0].equalsIgnoreCase("remove")
+					|| args[0].toLowerCase().equalsIgnoreCase("удалить")) {
+				if (args.length == 1) {
 					УдалитьПоследнийИзИБД(p, СИ);
 					СИ.УдалитьПоследнийЭлементМеню();
-				sender.sendMessage("Последний элемент меню удален");}
-				if(args.length>1){}
+					sender.sendMessage("Последний элемент меню удален");
 				}
-						
+				if (args.length > 1) {
+				}
+			}
+
 			return true;
-		} 
-		return false; 
+		}
+		return false;
 	}
 
 	private void УдалитьПоследнийИзИБД(Player p, СостояниеИгрока СИ) {
 		ArrayList<ItemStack> Menu = СИ.МенюИгрока;
-		ItemMeta im = Menu.get(Menu.size()-1).getItemMeta();
+		ItemMeta im = Menu.get(Menu.size() - 1).getItemMeta();
 		try {
 			sqlite.execute("delete from PlayerMenu where playername = '"
-					+ p.getName().replace('\'', ' ')
-					+ "' and command = '" + im.getLore().get(0) + "';");
+					+ p.getName().replace('\'', ' ') + "' and command = '"
+					+ im.getLore().get(0) + "';");
 		} catch (Exception e) {
-			//e.printStackTrace();
+			// e.printStackTrace();
 		}
 	}
 
@@ -106,10 +128,13 @@ public class Main extends JavaPlugin  {
 		try {
 			sqlite.execute("insert into PlayerMenu (playername, command, name) VALUES('"
 					+ p.getName().replace('\'', ' ')
-					+ "','" + s + "','" 
-					+ a + "');");
+					+ "','"
+					+ s
+					+ "','"
+					+ a
+					+ "');");
 		} catch (Exception e) {
-			//e.printStackTrace();
+			// e.printStackTrace();
 		}
 	}
 
