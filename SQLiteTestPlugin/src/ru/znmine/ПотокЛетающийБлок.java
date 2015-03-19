08ded9e68ab0d5e6c990a502aeb20a22194b303d
znsoft <zn@bk.ru>
2015-03-20 02:31:41 +1000

    табличка поворачивается

--- SQLiteTestPlugin/src/ru/znmine/ПотокЛетающийБлок.java
+++ SQLiteTestPlugin/src/ru/znmine/ПотокЛетающийБлок.java
@@ -10,8 +10,8 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
-//import org.bukkit.entity.Arrow;
 import org.bukkit.util.Vector;
 
 /**
@@ -24,6 +24,8 @@ public class ПотокЛетающийБлок implements Runnable {
 	World Мир;// = loc.getWorld();
 	Block Блок;
 	Material Материал;
+	float pitch;
+	float yaw;
 	Logger log = Logger.getLogger("Minecraft");
 	/**
 	 * 
@@ -43,6 +45,15 @@ public class ПотокЛетающийБлок implements Runnable {
 	@Override
 	public void run() {
 		
+		
+//		for (Location l : signs.toArray(new Location[0])) {
+//			final BlockState bs = l.getBlock().getState();
+//			if (bs instanceof Sign) {
+//				if (plugin.config.isClockwise) {
+//					if (l.getBlock().getData() == 0xF) {
+//						l.getBlock().setData((byte) 0);
+		
+		
 	//	double c = Math.cos((double)(Время++) / 55.0 );
 	//	double s = Math.sin((double)(Время++) / 50.0 );
 		Блок = Мир.getBlockAt(Положение);
@@ -51,7 +62,7 @@ public class ПотокЛетающийБлок implements Runnable {
 	//	Блок = Мир.getBlockAt(Положение);
 	//	Материал = Блок.getType();
 		Блок.setType(Material.SIGN_POST);
-	
+		
 		double МинимальноеРасстояние = 13.0,Расстояние;
 		Player НайденИгрок = null;
 		for(Player player : Bukkit.getServer().getOnlinePlayers()){
@@ -61,12 +72,22 @@ public class ПотокЛетающийБлок implements Runnable {
 
 		
 		if(НайденИгрок == null)return;
+
 		//log.info(НайденИгрок.getName()+"  "+ String.valueOf(МинимальноеРасстояние));
 		//@SuppressWarnings("unused")
 	//	Расположение Турель = new Расположение(Положение);
 		Location l = НайденИгрок.getLocation().clone().subtract(Положение);
 		Vector Вектор = (new Vector(l.getX(), l.getY()+МинимальноеРасстояние * 0.35, l.getZ())).normalize();
 		//log.info(Вектор.toString());
+		setDirection(Вектор);
+		
+//		Положение.setYaw((float) yaw);
+		if (Блок.getState() instanceof Sign) {
+			Блок.setData((byte)(yaw));
+			Sign sign = (Sign)Блок.getState();
+			//sign.setData((byte)yaw*64);
+			sign.update();
+		}	
 		
 		//if()
 		
@@ -84,5 +105,39 @@ public class ПотокЛетающийБлок implements Runnable {
 //	 loc.distance(o);
 //	b.setType(Material.SAND);
 	}
+	
+	
+	 /**
+     * Sets the {@link #getYaw() yaw} and {@link #getPitch() pitch} to point
+     * in the direction of the vector.
+     */
+    public Location setDirection(Vector vector) {
+        /*
+         * Sin = Opp / Hyp
+         * Cos = Adj / Hyp
+         * Tan = Opp / Adj
+         *
+         * x = -Opp
+         * z = Adj
+         */
+        final double _2PI = 2 * Math.PI;
+        final double x = vector.getX();
+        final double z = vector.getZ();
+
+        if (x == 0 && z == 0) {
+            pitch = vector.getY() > 0 ? -90 : 90;
+            return Положение;
+        }
+
+        double theta = Math.atan2(-x, z);
+        yaw = (float) Math.toDegrees((theta + _2PI) % _2PI);
+
+        double x2 = x * x;
+        double z2 = z * z;
+        double xz = Math.sqrt(x2 + z2);
+        pitch = (float) Math.toDegrees(Math.atan(-vector.getY() / xz));
+
+        return Положение;
+    }
 
 }
